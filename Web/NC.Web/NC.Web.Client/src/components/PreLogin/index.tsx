1dd356741b542e8a9e1b6e86b71be55fff93a679
Çağrı Cesur <cagricesur@gmail.com>
2026-05-07 01:06:03 +0300

    background update

--- Web/NC.Web/NC.Web.Client/src/components/PreLogin/index.tsx
+++ Web/NC.Web/NC.Web.Client/src/components/PreLogin/index.tsx
@@ -17,59 +17,38 @@ interface IPreLoginProps extends PropsWithChildren {
 
 export const PreLogin: React.FunctionComponent<IPreLoginProps> = (props) => {
   return (
-    <>
-      <ReactBits.FloatingLines
-        enabledWaves={["top", "middle", "bottom"]}
-        lineCount={5}
-        lineDistance={5}
-        bendRadius={5}
-        bendStrength={-0.5}
-        interactive={false}
-        parallax={false}
-        linesGradient={[
-          "#81e1ab",
-          "#70dc9f",
-          "#45d282",
-          "#37cf7a",
-          "#71d9f0",
-          "#3ecae9",
-          "#30c6e7",
-          "#16c3e7",
-        ]}
-      />
-      <Flex h="100dvh" justify="center" align="center" p="md">
-        <Paper radius="md" p="xl" className={classNames.root}>
-          <Stack align="stretch" gap={0}>
-            <Group justify="center">
-              <Image src={logo} h={64} w={64} />
-            </Group>
-            <ReactBits.GradientText
-              className={classNames.brand}
-              colors={["#70DC9F", "#3ECAE9"]}
-              animationSpeed={8}
-              yoyo
-            >
-              NoobzCord
-            </ReactBits.GradientText>
+      <Flex className={classNames.screen}>
+          <Paper radius="lg" shadow="xl" className={classNames.root}>
+        <Stack align="stretch" gap={0}>
+          <Group justify="center">
+            <Image src={logo} h={64} w={64} alt="NoobzCord" />
+          </Group>
+          <ReactBits.GradientText
+            className={classNames.brand}
+            colors={["#70DC9F", "#3ECAE9"]}
+            animationSpeed={8}
+            yoyo
+          >
+            NoobzCord
+          </ReactBits.GradientText>
 
-            <Divider
-              my={32}
-              label={
-                props.hasColorSchemeSwitcher || props.hasLanguageSwitcher ? (
-                  <Group justify="center">
-                    {props.hasLanguageSwitcher && (
-                      <LanguageSwitcher onChange={props.onLanguageSwitched} />
-                    )}
-                    {props.hasColorSchemeSwitcher && <ColorSchemeSwitcher />}
-                  </Group>
-                ) : null
-              }
-            />
+          <Divider
+            my={32}
+            label={
+              props.hasColorSchemeSwitcher || props.hasLanguageSwitcher ? (
+                <Group justify="center">
+                  {props.hasLanguageSwitcher && (
+                    <LanguageSwitcher onChange={props.onLanguageSwitched} />
+                  )}
+                  {props.hasColorSchemeSwitcher && <ColorSchemeSwitcher />}
+                </Group>
+              ) : null
+            }
+          />
 
-            {props.children}
-          </Stack>
-        </Paper>
-      </Flex>
-    </>
+          {props.children}
+        </Stack>
+      </Paper>
+    </Flex>
   );
 };
